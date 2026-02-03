9c7e8fc386d1a7a618bbfb4ebd4afd7d42496740
Jakub-Lukac <jakub.lukac81@gmail.com>
2026-02-03 11:59:23 +0000

    service added for count

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { Child1Component } from './child1/child1.component';
+import { Child2Component } from "./child2/child2.component";
+import { CounterService } from './counter.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Child1Component, Child2Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'osd-week3-signals';
+  counterService = inject(CounterService);
+  constructor() {}
+  
+  // Access the signals directly
+  count = this.counterService.count;
+  doubleCount = this.counterService.doubleCount;
+  
+  increment() {
+    this.counterService.increment();
+  }
+  
+  decrement() {
+    this.counterService.decrement();
+  }
 }
---
+++ src/app/counter.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, signal, computed } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CounterService {
+  count = signal(0); // writable signal
+  
+  doubleCount = computed(() => {
+    console.log('Calculating doubleCount:', this.count() * 2);
+    return this.count() * 2;
+  }); // computed signal -> readonly
+
+  increment() {
+    this.count.update(value => value + 1);
+    console.log('Count after increment:', this.count());
+  }
+
+  decrement() {
+    this.count.update(value => value > 0 ? value - 1 : 0);
+    console.log('Count after decrement:', this.count());
+  }
+
+  getCount() {
+    console.log('Getting count:', this.count());
+    return this.count();
+  }
+}
